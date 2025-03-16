3eb3cb5c32b80de545419aa86026959664339407
Aleks-Data <sumarokovav1977@gmail.com>
2025-03-16 20:29:18 +0200

    Домашняя 35
    Только первое задание, как говорили на уроке.

---
+++ HW35/Task_1.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_response(url):
+    response = requests.get(url)
+    return response
+
+url = "https://en.wikipedia.org/wiki/Tree"
+response = get_response(url)
+
+print("Status Code:", response.status_code)
+print("Response Text:", response.text)
+print("Response Headers:", response.headers)
