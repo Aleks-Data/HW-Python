8f60e85a3f9f40142af587b38f84a4442cd32f0b
Aleks-Data <sumarokovav1977@gmail.com>
2025-03-22 13:49:37 +0200

    Update Task_1.py

--- HW34/Task_1.py
+++ HW34/Task_1.py
@@ -1,3 +1,6 @@
+# Напишите функцию extract_emails(text), которая извлекает все адреса
+# электронной почты из заданного текста и возвращает их в виде списка.
+
 import re
 
 text = "Contact us at in%fo@example.com or sup-p+ort@example.com for assistance."
