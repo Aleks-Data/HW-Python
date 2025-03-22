7f09fcd0414c1b16648a367ad0a16246b3643445
Aleks-Data <sumarokovav1977@gmail.com>
2025-03-22 13:40:44 +0200

    Task_1.py

--- HW35/Task_1.py
+++ HW35/Task_1.py
@@ -1,8 +1,19 @@
+# 1. Напишите функцию get_response(url),
+# которая отправляет GET-запрос по заданному
+# URL-адресу и возвращает объект ответа.
+# Затем выведите следующую информацию об ответе:
+# - Код состояния (status code)
+# - Текст ответа (response text)
+# - Заголовки ответа (response headers)
+
+
 import requests
+
 
 def get_response(url):
     response = requests.get(url)
     return response
+
 
 url = "https://en.wikipedia.org/wiki/Tree"
 response = get_response(url)
